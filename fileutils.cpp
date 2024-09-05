3590b2588f982138242c1034da87bc18d0300c8a
Don Woodraska <don.woodraska@lasp.colorado.edu>
2024-09-05 21:50:57 +0000

    removing fileutils

--- fileutils.cpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-
-// Function returns false if filename is empty
-bool isValidFilename(const std::string& filename) {
-    return !filename.empty();
-}
--- fileutils.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FILEUTILS_H
-#define FILEUTILS_H
-
-#include <string>
-
-// Function declarations
-bool isValidFilename(const std::string& filename);
-
-#endif // FILEUTILS_H
