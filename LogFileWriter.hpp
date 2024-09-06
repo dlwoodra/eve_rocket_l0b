91a0d06f27a97ae7f27094ae0c8251e8d11f9d86
Don Woodraska <don.woodraska@lasp.colorado.edu>
2024-09-06 21:33:40 +0000

    switch to precompiled header

--- LogFileWriter.hpp
+++ LogFileWriter.hpp
@@ -3,8 +3,9 @@
 
 // uses Singleton pattern
 
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/basic_file_sink.h>
+//#include <spdlog/spdlog.h>
+//#include <spdlog/sinks/basic_file_sink.h>
+#include "spdlog_pch.hpp"
 #include <string>
 #include <chrono>
 #include <iomanip>
