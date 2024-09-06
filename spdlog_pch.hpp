50a467189a3798385ecd7eecdb7bc8f42010fb2f
Don Woodraska <don.woodraska@lasp.colorado.edu>
2024-09-06 21:35:25 +0000

    initial commit of precompiled header

---
+++ spdlog_pch.hpp
@@ -0,0 +1,8 @@
+// spdlog_pch.hpp
+// Pre-compiled header, created by DLW 09/06/24
+// using spdlog headers compilation usign makeall target takes 1:24
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <vector>
+#include <iostream>
