0eb5f87636d242c59e6d08e30151d4050f9c7b59
Don Woodraska <don.woodraska@lasp.colorado.edu>
2024-09-29 23:07:16 +0000

    add SHK FITS writing

--- FITSWriter.hpp
+++ FITSWriter.hpp
@@ -14,50 +14,52 @@
 
 #include "/usr/local/include/fitsio.h"
 
-// Custom deleter for fitsfile* to be used with std::unique_ptr
-struct FITSFileDeleter {
-    void operator()(fitsfile* fptr) const {
-        if (fptr) {
-            int status = 0;
-            fits_close_file(fptr, &status);
-            if (status) {
-                fits_report_error(stderr, status);
-            }
-        }
-    }
-};
-
 class FITSWriter {
 public:
     FITSWriter();
     ~FITSWriter();
 
-    // Write the packet data to the appropriate FITS file based on APID and timestamp
-    //bool writePacketToFITS(const std::vector<uint8_t>& packet, uint16_t apid, double timestamp);
-
-    bool writeMegsFITS(const MEGS_IMAGE_REC& megsStructure, uint16_t apid, const std::string& extname);
+    // interfaces to common writeMegsFITS
     bool writeMegsAFITS(const MEGS_IMAGE_REC& megsStructure);
     bool writeMegsBFITS(const MEGS_IMAGE_REC& megsStructure);
-    
+    // interfaces to write the other packet types
     bool writeMegsPFITS(const MEGSP_PACKET& megsPStructure);
     bool writeESPFITS(const ESP_PACKET& ESPStructure);
+    bool writeSHKFITS(const SHK_PACKET& SHKStructure);
 
 private:
-    // Helper function to create a FITS filename based on APID and timestamp
+    // common MEGS image writing
+    bool writeMegsFITS(const MEGS_IMAGE_REC& megsStructure, uint16_t apid, const std::string& extname);
+
+    // common function to create a FITS filename based on APID and timestamp
+    // that calls the cfitsio API fits_create_file function
     std::string createFITSFilename(uint16_t apid, double timestamp);
 
-    // Helper function to initialize a FITS file
+    // common function to initialize a FITS file
     bool initializeFITSFile(const std::string& filename, fitsfile*& fptr);
 
+    //handler for errors - all errors are fatal
+    void checkFitsStatus(int status);
 
-    // Helper function to write data to a FITS file
-    bool writeDataToFITS(fitsfile* fptr, const std::vector<uint8_t>& data);
+    // converts a vector of strings to a vector of char* for arays of FITS types
+    std::vector<char*> convertToCharPtrArray(const std::vector<std::string>& vec);
 
-    // Map to store FITS file pointers for each APID
-    std::unordered_map<std::string, std::unique_ptr<fitsfile, FITSFileDeleter>> fitsFileMap;
+    // converts an array of strings into a vector of char* for scalar FITS types
+    // this might be replacable by convertToCharPtrArray, relies on new
+    std::vector<char*> convertTypesToCharPtrArray(const std::string& types);
+    std::vector<char*> convertTypesToTFormPtrArray(const std::string& types, const std::vector<int>& lengths);
+    // use to free memory allocated in convertTypesTo... functions
+    void freeCharPtrArray(std::vector<char*>& array);
 
-    // Utility function to close all FITS files
-    void closeAllFITSFiles();
+    bool writeMegsFITSImgHeader(fitsfile* fptr, const MEGS_IMAGE_REC& megsStructure, int32_t& status);
+    int writeBinaryTable(const std::string& filename, const void* data, int columns, 
+        const std::vector<std::string>& names, const std::string& types, const std::vector<int>& columnLengths,
+        const std::vector<std::string>& units, bool useUnits, const std::string& extname);
+    int writeMegsFITSBinaryTable(const std::string& filename, const MEGS_IMAGE_REC& megsStructure,
+        const std::string& extname, uint16_t apid);    
+    int writeMegsPFITSBinaryTable(const std::string& filename, const MEGSP_PACKET& megsPStructure);
+    int writeESPFITSBinaryTable(const std::string& filename, const ESP_PACKET& ESPStructure);
+    int writeSHKFITSBinaryTable(const std::string& filename, const SHK_PACKET& SHKStructure);
 };
 
 #endif // FITSWRITER_HPP
