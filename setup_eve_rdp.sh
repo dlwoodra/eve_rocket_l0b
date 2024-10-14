c4400ac580745fed02815da84361b41921bfb622
Don Woodraska <don.woodraska@lasp.colorado.edu>
2024-10-14 22:49:19 +0000

    add bash setup

---
+++ setup_eve_rdp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# need to call using   . setup_eve_rdp.sh
+# if you then run tcsh it will inherit these environment variables
+
+export eve_data_root=./data/
+export eve_log_root=./logs/
+export eve_code_root=./
+
+export eve_data_record=${eve_data_root}record/
+export eve_data_tlm=${eve_data_record}
+export eve_data_l0b=${eve_data_root}level0b/
+export eve_data_quicklook=${eve_data_root}quicklook/
+
+export eve_cal_data=${eve_data_root}cal/
+
