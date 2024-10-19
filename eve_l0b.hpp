6e4f5dacbcc0ef14c9e78204973f2eefc35ce748
Don Woodraska <don.woodraska@lasp.colorado.edu>
2024-10-19 21:46:42 +0000

    remove commented variables

--- eve_l0b.hpp
+++ eve_l0b.hpp
@@ -441,11 +441,8 @@ struct ProgramState {
 	long dataGapsESP = 0;
 	long dataGapsSHK = 0;
 	ESP_PACKET esp;
-	//bool espUpdated = true;
 	MEGSP_PACKET megsp;
-	//bool megsPUpdated = true;
 	SHK_PACKET shk;
-	//bool shkUpdated = true;
 };
 
 
