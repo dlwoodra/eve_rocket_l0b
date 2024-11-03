73fbdeb42ee93ed18ec562f1e2ed8a142941c203
Don Woodraska <don.woodraska@lasp.colorado.edu>
2024-11-03 21:39:12 +0000

    updated STANDRD_PACKET_LENGTHs

--- CCSDSReader.hpp
+++ CCSDSReader.hpp
@@ -19,9 +19,9 @@ constexpr uint32_t SYNC_MARKER = 0x1ACFFC1D; // sync marker
 constexpr uint32_t BSWAP_SYNC_MARKER = 0x1DFCCF1A;
 constexpr size_t PACKET_HEADER_SIZE = 6;     // CCSDS packet primary header size in bytes
 constexpr uint16_t STANDARD_MEGSAB_PACKET_LENGTH = 1761; // value from packet (one less than size)
-constexpr uint16_t STANDARD_MEGSP_PACKET_LENGTH = 25; // apid 604
-constexpr uint16_t STANDARD_ESP_PACKET_LENGTH = 89; // apid 605
-constexpr uint16_t STANDARD_HK_PACKET_LENGTH = 265; // apid 606
+constexpr uint16_t STANDARD_MEGSP_PACKET_LENGTH = 1761; //25; // apid 604
+constexpr uint16_t STANDARD_ESP_PACKET_LENGTH = 1761; //89; // apid 605
+constexpr uint16_t STANDARD_HK_PACKET_LENGTH = 1761; //265; // apid 606
 // David's definitions of APIDs
 constexpr uint16_t MEGSA_APID = 601; // same as SDO EVE
 constexpr uint16_t MEGSB_APID = 602; // same as SDO EVE
