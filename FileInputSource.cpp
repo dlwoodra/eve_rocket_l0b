b4858c78001ad92d7b4d258df95571cbf935d8c9
Don Woodraska <don.woodraska@lasp.colorado.edu>
2024-11-05 21:51:47 +0000

    add ability to reprocess gzip files

--- FileInputSource.cpp
+++ FileInputSource.cpp
@@ -1,31 +1,57 @@
 #include "FileInputSource.hpp"
 
 FileInputSource::FileInputSource(const std::string& filename) 
-    : filename(filename) {}
+    : filename(filename), isCompressed(checkIfCompressed()) {}
 
 FileInputSource::~FileInputSource() {
     close();
 }
 
+bool FileInputSource::checkIfCompressed() const {
+    // Check if filename ends with ".gz" (case sensitive)
+    return filename.size() >= 3 && filename.substr(filename.size() - 3) == ".gz";
+}
+
 bool FileInputSource::open() {
-    fileStream.open(filename, std::ios::binary);
-    return fileStream.is_open();
+    if (isCompressed) {
+        gzFileStream = gzopen(filename.c_str(), "rb"); // Open gzip file
+        return gzFileStream != nullptr;
+    } else {
+        fileStream.open(filename, std::ios::binary); // Open regular file
+        return fileStream.is_open();
+    }
 }
 
 void FileInputSource::close() {
-    if (fileStream.is_open()) {
-        fileStream.close();
+    if (isCompressed) {
+        if (gzFileStream) {
+            gzclose(gzFileStream);
+            gzFileStream = nullptr;
+        }
+    } else {
+        if (fileStream.is_open()) {
+            fileStream.close();
+        }
     }
 }
 
 bool FileInputSource::read(uint8_t* buffer, size_t size) {
     if (isOpen()) {
-        fileStream.read(reinterpret_cast<char*>(buffer), size);
-        return fileStream.gcount() == static_cast<std::streamsize>(size);
+        if (isCompressed) {
+            if (gzFileStream) {
+                int bytesRead = gzread(gzFileStream, buffer, size);
+                return bytesRead == static_cast<int>(size);
+            }
+        } else {
+            if (fileStream.is_open()) {
+                fileStream.read(reinterpret_cast<char*>(buffer), size);
+                return fileStream.gcount() == static_cast<std::streamsize>(size);
+            }
+        }
     }
     return false;
 }
 
 bool FileInputSource::isOpen() const {   // Implementing isOpen method
-    return fileStream.is_open();
+    return isCompressed ? (gzFileStream != nullptr) : fileStream.is_open();
 }
--- FileInputSource.hpp
+++ FileInputSource.hpp
@@ -3,6 +3,8 @@
 
 #include "InputSource.hpp"
 #include <fstream>
+#include <string>
+#include <zlib.h>
 
 class FileInputSource : public InputSource {
 public:
@@ -10,16 +12,17 @@ public:
     ~FileInputSource();
 
     bool open() override;
-
     void close() override;
-
     bool read(uint8_t* buffer, size_t size) override;
-
     bool isOpen() const override;
 
 private:
-    std::ifstream fileStream;
+    std::ifstream fileStream; //for uncompressed files
+    gzFile gzFileStream = nullptr; //for compressed files
     std::string filename;
+    bool isCompressed = false;
+    
+    bool checkIfCompressed() const;
 };
 
 #endif // FILE_INPUT_SOURCE_HPP
