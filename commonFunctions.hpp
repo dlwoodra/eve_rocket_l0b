117841cdd5da73e5c45dfcb8ee7375783336702f
Don Woodraska <don.woodraska@lasp.colorado.edu>
2024-11-07 22:30:26 +0000

    add declaration for transposeImageTo1D

--- commonFunctions.hpp
+++ commonFunctions.hpp
@@ -19,7 +19,7 @@
 //#include <iostream> // included in CCSDSReader.hpp
 #include <algorithm> // needed for std::copy
 #include <string>
-#include <mutex>
+//#include <mutex>
 #include <sys/stat.h> // For mkdir
 #include <sys/types.h> // For mode_t
 #include <errno.h>     // For errno
@@ -27,7 +27,7 @@
 
 //prototypes
 bool isValidFilename(const std::string& filename);
-std::vector<uint16_t> transposeImage(const uint16_t image[MEGS_IMAGE_WIDTH][MEGS_IMAGE_HEIGHT]);
+//std::vector<uint16_t> transposeImage(const uint16_t image[MEGS_IMAGE_WIDTH][MEGS_IMAGE_HEIGHT]);
 
 void processPackets(CCSDSReader& pktReader, std::unique_ptr<RecordFileWriter>& recordWriter, bool skipRecord);
 void processOnePacket(CCSDSReader& pktReader, const std::vector<uint8_t>& packet);
@@ -48,5 +48,7 @@ void printBytesToStdOut(const uint8_t* array, uint32_t start, uint32_t end);
 void printUint16ToStdOut(const uint16_t* image, size_t size, size_t count);
 
 bool create_directory_if_not_exists(const std::string& dirPath);
+bool create_single_directory(const std::string& path);
+std::vector<uint16_t> transposeImageTo1D(const uint16_t image[MEGS_IMAGE_HEIGHT][MEGS_IMAGE_WIDTH]);
 
 #endif // COMMONFUNCTIONS_H
