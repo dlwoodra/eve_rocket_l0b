864f91c1fd670cbae4b37a8285470a0df0906ca4
Don Woodraska <don.woodraska@lasp.colorado.edu>
2024-11-07 23:12:56 +0000

    adding prototypes

--- commonFunctions.hpp
+++ commonFunctions.hpp
@@ -50,5 +50,17 @@ void printUint16ToStdOut(const uint16_t* image, size_t size, size_t count);
 bool create_directory_if_not_exists(const std::string& dirPath);
 bool create_single_directory(const std::string& path);
 std::vector<uint16_t> transposeImageTo1D(const uint16_t image[MEGS_IMAGE_HEIGHT][MEGS_IMAGE_WIDTH]);
+uint32_t payloadBytesToUint32(const std::vector<uint8_t>& payload, const int32_t offsetByte);
+uint32_t payloadToTAITimeSeconds(const std::vector<uint8_t>& payload);
+uint32_t payloadToTAITimeSubseconds(const std::vector<uint8_t>& payload);
+//void populateStructureTimes(MEGS_IMAGE_REC& oneStructure, const std::vector<uint8_t>& payload);
+//void populateStructureTimes(MEGSP_PACKET& oneStructure, const std::vector<uint8_t>& payload);
+//void populateStructureTimes(ESP_PACKET& oneStructure, const std::vector<uint8_t>& payload);
+//void populateStructureTimes(SHK_PACKET& oneStructure, const std::vector<uint8_t>& payload);
+void countSaturatedPixels(const uint16_t image[MEGS_IMAGE_HEIGHT][MEGS_IMAGE_WIDTH],
+                          uint32_t& saturatedPixelsTop,
+                          uint32_t& saturatedPixelsBottom,
+                          bool testPattern);
+
 
 #endif // COMMONFUNCTIONS_H
